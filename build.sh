f9851d028add8ad5f473708f73d9539da41a07f1
Chang Wei <m11002815@gapps.ntust.edu.tw>
2021-10-26 06:07:23 -0700

    extract variable: CMAKE_BUILD_TYPE

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+CMAKE_BUILD_TYPE=RelWithDebInfo
 rm -rf ./CMakeCache.txt ./CMakeFiles/ ./build
-cmake -B ./build -DCMAKE_BUILD_TYPE=RelWithDebInfo && cmake --build ./build -v --config RelWithDebInfo
+cmake -B ./build -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} && cmake --build ./build -v --config ${CMAKE_BUILD_TYPE}
